b887a7facf1178ffec6aa4732532325399f11aad
Santiago <santiagoreyesmarin1986@gmail.com>
2022-06-21 16:03:34 -0500

    12

--- src/App.js
+++ src/App.js
@@ -39,6 +39,8 @@ export const App = () => {
           <p>Linkedin:<a href='https://www.linkedin.com/in/santiagoa31219' target="blank"><i className="fab fa-linkedin"></i></a> </p>
           <p>Correo: <span>Santiagoreyesmarin1986@gmail.com</span></p>
           <p>Cv: <span><a href='https://xd.adobe.com/view/a8382433-4a35-4faf-9349-bb6f5aac716b-fd6b/?hints=off' target="blank">here!</a></span></p>
+          <p>Cv: <span><a className='PDF' href='https://github.com/SantiagoJSdev/aboutNew/files/8950914/Santiago-Pdf.pdf' target="blank">PDF</a></span></p>
+
           <p>Idioma: <span>English A2</span></p>
           <p>Hobbies: <span>Cine</span><span>Games</span><span>Baloncesto</span></p>
         </div>
@@ -48,10 +50,10 @@ export const App = () => {
       </section>
       <section className='home__right'>
         <div className='right__container'>
-             <h1>¡Hola! estoy motivado por el desarrollador web. Mis habilidades y conocimientos están enfocadas en JavaScript,
-               React, Redux, HTML5, CSS3, Node. Quisiera cubrir una posición en procesos de front-end con JavaScript usando React y
-                en el back-end con Node.js. Soy una persona enfocada en mantenerse actualizado en nuevas tecnologías, actualmente trabajo para <span><a href='https://www.linkedin.com/company/tecnopac/' target="blank">Tecnopac</a></span>, como Full Stack Junior, donde tengo la oportunidad de aprender cada día y desarrollar mis habilidades.</h1>
-        
+          <h1>¡Hola! estoy motivado por el desarrollador web. Mis habilidades y conocimientos están enfocadas en JavaScript,
+            React, Redux, HTML5, CSS3, Node. Quisiera cubrir una posición en procesos de front-end con JavaScript usando React y
+            en el back-end con Node.js. Soy una persona enfocada en mantenerse actualizado en nuevas tecnologías, actualmente trabajo para <span><a href='https://www.linkedin.com/company/tecnopac/' target="blank">Tecnopac</a></span>, como Full Stack Junior, donde tengo la oportunidad de aprender cada día y desarrollar mis habilidades.</h1>
+
           <div className='right__container--academica'>
             <h1>Formación académica:</h1>
             <p>Universitaria: <span>Técnico Superior en Informática.</span></p>
@@ -59,8 +61,8 @@ export const App = () => {
           </div>
           <div className='right__container--academica'>
             <h1>Experiencia Laboral:</h1>
-            <p>Académica: <span><a href='https://pg-henry.vercel.app/' target="blank"> E-commerce, proyecto grupal Bootcamp soy henry.</a> </span></p>
-            <p>Tecnopac: <span><a href='https://una-idea.herokuapp.com/' target="blank">Proyecto 1Idea - Desarrollo inicial del prototipo webApp para una empresa de consultoría, tecnologías
+            <p>Académica: <span><a href='https://pg-henry.vercel.app/' target="blank"  > E-commerce, proyecto grupal Bootcamp soy henry.</a> </span></p>
+            <p>Tecnopac: <span><a href='https://una-idea.herokuapp.com/' target="blank"  >Proyecto 1Idea - Desarrollo inicial del prototipo webApp para una empresa de consultoría, tecnologías
               utilizadas en el frontend: React, en los estilos css3 vanila y en el backend: node.js, Express, Mongoose, mongodb.</a></span></p>
             <p>Tecnopac: <span>Proyecto Estuhouse - Desarrollo webApp para una empresa de alquiler de apartamentos y habitaciones para estudiantes universitarios, tecnologías
               utilizadas en el frontend: React, en los estilos css3 vanila y en el backend: node.js, Express, Mongoose, mongodb. </span></p>
